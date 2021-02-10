9752a95386bada42302a6b5b0a493827fffaf774
johann43 <emman.soliano@gmail.com>
2021-02-10 21:08:15 +0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+          fugit nam asperiores facere et inventore aperiam, pariatur optio
+          maiores aliquid nisi dolorum omnis quia sunt placeat alias repudiandae
+          distinctio nesciunt ut temporibus, vel ullam dignissimos. Ducimus
+          necessitatibus, temporibus non suscipit qui, incidunt odit libero
+          fugiat impedit dolore cumque a? Maiores aspernatur possimus, voluptate
+          ducimus, aliquam neque in fugiat, nulla architecto provident eos?
+          Ullam, eum. Soluta magni facere ea reiciendis magnam illum officiis
+          distinctio neque corporis rem error harum a, architecto assumenda
+          eligendi praesentium totam ipsa facilis eum ullam itaque iure nemo
+          culpa adipisci! Harum voluptatem cumque at perspiciatis impedit ipsum.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
+          veritatis minima, corrupti velit ipsam earum quo architecto omnis,
+          repudiandae optio autem magni nam eos id vel. Modi facere minima eos
+          odio. Maxime, cupiditate provident. Ab at aspernatur corrupti aliquam
+          totam, dignissimos necessitatibus incidunt vitae deleniti quibusdam
+          doloremque ea mollitia obcaecati?
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default About
